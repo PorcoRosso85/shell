4a88054fb8d9cdb69f270e5f4a374528161b0979
PorcoRosso85 <1.is.universe@gmail.com>
2023-08-01 12:24:59 +0900

    nvim, docker in os

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y
 bash <(curl -L https://nixos.org/nix/install) --daemon
+sudo apt-get install gh -y
+
+sudo gh auth login
+
+sudo gh repo clone PorcoRosso85/shell
+bash ./nvim.sh
+bash ./docker.sh
